e9daf43fc4f468966a01bc48a515e51f4e2973e0
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:10:08 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,12 @@
+float_num =3.75                                     #converting a string to integer 
+converted_int=int(float_num )
+print("converted float to int:", converted_int)
+integer_num =15                                     #converting an integer to string 
+converted_str=str(integer_num )
+print("converted int to str:", converted_str)
+string_num="25"                                     #converting a string to integer
+converted_str_to_int= int(string_num)
+print("converted str to int:", converted_str_to_int)
+string_float =7.5                                   #converting a string to float    
+converted_str_to_float = float(string_float)
+print("converted str to float:", converted_str_to_float)
