03009754750ffbcbaf85e7ae759680fc162477a6
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:34:28 +0530

    Create persentcalc.py

---
+++ persentcalc.py
@@ -0,0 +1,7 @@
+a=input ("enter physics  no.")
+b=input ("enter chemistry no.")
+c=int(input ("enter maths no."))
+d=int(input ("enter english no."))
+e=int(input ("enter computer no."))
+f=a+b+c+d+e
+print("the percentage is",f/500*100)
