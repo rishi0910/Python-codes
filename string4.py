f5ef917a3d3256f03b0aba2d387ec6709ec13a59
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:37:55 +0530

    Create string4.py

---
+++ string4.py
@@ -0,0 +1,9 @@
+# input_str=input("Enter any string :")
+# reversed_str= input_str[::-1]
+# print(reversed_str)
+
+
+input_str=input("Enter an string : ")
+char ="l"
+count= input_str.count(char)
+print(count)
