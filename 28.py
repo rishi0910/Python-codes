b85c103d6c233bba850350b196f47081fb85b4de
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:10:31 +0530

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,19 @@
+#
+
+
+
+principal_amount=int(input("Enter Principal  "))
+interest_rate=float(input("Enter Interest rate  "))
+time_period=int(input("Enter Time period   "))
+simple_interest=principal_amount*interest_rate*time_period/100
+print("The Simple Interst is  " , simple_interest)
+
+
+#
+
+
+
+l=float(input("Enter length of rectangle  "))
+b=float(input("Enter Breath of the rectangle  "))
+Area=l*b 
+print("The Area of Rectangle is   ", Area)
