759ede9dcada59e89f36b48a752f4127b73b6556
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:21:47 +0530

    Create password.py

---
+++ password.py
@@ -0,0 +1,10 @@
+while True:
+    password = input("create a password (atleast 6 characters):")
+    if len(password)<6:
+        print("Password is too short! please try again.")
+        pass
+    else:
+         print("password set sucessfully")
+         break
+    
+     
