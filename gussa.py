2e3ad171f9682b506c44f5e0aabe20db71f36742
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:24:56 +0530

    Create gussa.py

---
+++ gussa.py
@@ -0,0 +1,8 @@
+h=input("kya aap mujse gussa ho yes/no : ")
+if h=="yes":
+    print("pleaseee gussa nahi ho naa .....")
+else:
+    print("Toh haso na budhuu itna sada hua sa muhhh kyuu bna rkha h ....")
+
+
+  
