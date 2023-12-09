2876e583f4a3a4da0cf936046b950d78c7bcdb0c
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:22:13 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,7 @@
+num1 =int(input("Enter A Start Value :"))
+num2=int(input("Enter A Stop Value :"))
+for i in range (num1,num2):
+     if i % 2==0:
+          print(i ," is Even ")
+     else:
+          print(i , " is Odd ")
