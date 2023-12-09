08c9901136eb1104630e8b8727ec92a00b7a8b53
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:10:59 +0530

    Create 29.py

---
+++ 29.py
@@ -0,0 +1,18 @@
+#
+
+
+cy=float(input("Enter current year  "))
+by=float(input("Enter your Birth Year  "))
+age= cy-by
+print("Your current Age is  ",age)
+
+
+
+
+#
+
+
+
+fn=(input("Enter you First Name "))
+ln=(input("Enter your Last Name"))
+print("Hello",fn ,ln ,"Welcomw to GLA University")
