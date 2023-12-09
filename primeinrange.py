70839aef274a142887a6bd24b3d7757bdb01b757
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:42:07 +0530

    Create primeinrange.py

---
+++ primeinrange.py
@@ -0,0 +1,11 @@
+def prime(n):
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+    
+x=int(input())
+y=int(input())
+for t in range(x,y+1):
+    if(prime(t)):
+     print(t)
