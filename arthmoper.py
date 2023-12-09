140b7454477f5fe5e39e7b36a93ecf759a6746b8
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:15:06 +0530

    Create arthmoper.py

---
+++ arthmoper.py
@@ -0,0 +1,16 @@
+a=float(input("Enter a no."))
+b=float(input("Enter a no."))
+sum= a+b
+sub= a-b
+product = a*b
+division = a/b
+modulus_remainder = a%b
+floor_division = a//b
+exponentation = a**b
+print("the sum is :", sum) 
+print("the subtraction is :", sub)
+print("the product is :", product)
+print("the division is :", division)
+print("the modulus is :", modulus_remainder )
+print("the floor division is :", floor_division)
+print("the exponentiation is :", exponentation)
