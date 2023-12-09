cf5442c1527758d1b8710fd1044922f9eb29c0a6
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:27:59 +0530

    Create listdeletion.py

---
+++ listdeletion.py
@@ -0,0 +1,18 @@
+mylist=["a","b","c"]
+mylist.remove("b")
+print(mylist)
+
+mylist1=["a","b","c"]
+mylist1.pop(1)
+print(mylist1)
+
+mylist2=["a","b","c"]
+mylist2.pop()
+print(mylist2)
+
+mylist3=["a","b","c"]
+del mylist3[0]
+
+mylist=["a","b","c"]
+mylist.clear()
+print(mylist3)
