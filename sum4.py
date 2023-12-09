2658fedf950c1b0f2be90d29b04130bc742eabfe
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:38:13 +0530

    Create sum4.py

---
+++ sum4.py
@@ -0,0 +1,6 @@
+x=int(input("enter a no."))
+y=int(input("enter a no."))
+print("the sum is ",x+y)
+x=input("enter a no.")                           # string concatenation 
+y=input("enter a no.")
+print("the sum is ",x+y)        
