c15df6aacb2b208924b4e8a1ab38b3ddaa9b88a4
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:19:03 +0530

    Create builtin.py

---
+++ builtin.py
@@ -0,0 +1,19 @@
+#Here are a few examples:
+
+
+#Absoulute value
+num=-10
+abs_num= abs(num)
+print("Absoulute value:", abs_num)
+
+#Length of a string or collection
+text = "Hello, Python!"
+length = len(text)
+print("Length of text:", length)
+
+# Maximum and Minimum
+numbers = [5, 10, 2, 8]
+max_num = max(numbers)
+min_num = min(numbers)
+print("Maximum", max_num)
+print("Minimum", min_num)
