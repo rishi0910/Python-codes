7d1c729999123543b7c3961069989fe991d738c0
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:13:14 +0530

    Create 29_5.py

---
+++ 29_5.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your Age "))
+if age >= 18:
+    print("You are eligile to Vote.")
+    if age >= 21:
+        print ("You are also eligible for Driving License.")
+else:
+    print("You are not eligible to Vote or Driving Lisence.")
