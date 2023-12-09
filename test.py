48b7c5e2ff46cdac38b873801c82e196ece28f71
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:39:25 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+# x=10
+# while x>5:
+#     x-=1
+#     if x==7:
+#         break
+#     print (x , end=" ")
+# else:
+#     print("Loop completed")
+
+
+x=3
+for i in range(1,x+1):
+    print(""*(x-i)+"*"*(2*i-1))
