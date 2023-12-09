c217e293ecaeb39dc28f940beb9a8ae6d7ec3244
rishi0910 <142798577+rishi0910@users.noreply.github.com>
2023-12-09 08:14:09 +0530

    Create greatest.py

---
+++ greatest.py
@@ -0,0 +1,10 @@
+a = float(input("Enter a No. "))
+b = float(input("Enter a No. "))
+if a > b:
+    print(a, "Is greater than", b )
+elif a < b:
+    print(a, "Is lesser than", b )
+elif a == b:
+    print(a, " Is equal to" , b)
+    
+    
